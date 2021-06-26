ef426655eaa4c0327fd66d1e3fb8e7ae1c39e9b0
sachio <sachio.engineer@gmail.com>
2021-06-26 12:19:15 +0900

    :sparkles: Add blog機能追加

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,21 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path,
+      notice: "ブログを作成しました"
+    else
+      render :new
+    end
+  end
+  private
+  def blog_params
+    prams.require(:blog).permit(:title, :content)
+  end
+end
