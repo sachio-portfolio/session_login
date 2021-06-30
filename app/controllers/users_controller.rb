ce53d1e60aed127c32cbfd3ad9fe3211b8c6aba1
sachio <sachio.engineer@gmail.com>
2021-06-30 22:53:41 +0900

    :sparkles: Add フォロー機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
@@ -13,6 +16,8 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @following = @user.following
+    @followers = @user.followers
   end
   private
   def user_params
